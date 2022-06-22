c25c8f0dc9e074a807c843eff2e301784e9d697a
elqebtan <61181575+Elqeb@users.noreply.github.com>
2022-06-22 09:04:03 +0800

    pull  test

--- src/main/java/com/elqeb/demo/test/GitTest.java
+++ src/main/java/com/elqeb/demo/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("github push  test");
+        System.out.println("github pull  test");
     }
 }
